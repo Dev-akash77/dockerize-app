1986d73e44196f75e4c2190aca71ca75fe58e62c
Akash Biswas <akashrahul2006@gmail.com>
2025-07-15 23:04:00 +0530

    redis added

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const User = require('./dbmodel.js');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const User = require("./dbmodel.js");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('✅ MongoDB connected'))
-.catch((err) => console.error('❌ Connection error:', err));
+// ! redis connect
+const Redis = require('ioredis');
 
-app.get('/', async (req, res) => {
+const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
+ 
+
+// ! data base congigaraion
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("Connection error:", err));
+
+app.get("/", async (req, res) => {
   try {
+    const cachekey= "all_user";
+    const cache_data = await redis.get(cachekey);
+
+    if (cache_data) {
+       return res.status(200).json({cache_data:JSON.parse(cache_data)});
+    }
     const users = await User.find();
-    res.status(200).json(users);
+    await redis.set(cachekey, JSON.stringify(users), "EX", 60);
+    res.status(200).json(users); 
   } catch (err) {
-    res.status(500).json({ message: 'Server Error', error: err.message });
+    res.status(500).json({ message: "Server Error", error: err.message });
   }
 });
 
